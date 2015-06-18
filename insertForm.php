3e24da97e2d9c762cb1f40a02d49f5d089e0f658
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-06-18 19:04:23 +0200

    Update insertForm.php

--- insertForm.php
+++ insertForm.php
@@ -9,6 +9,7 @@
     <form method="POST" action="/insertDB.php">
       <input type="text" name="firstname" placeholder="firstname" />
       <input type="text" name="lastname"  placeholder="lastname" />
+      <input type="text" name="password" placeholder="password">
     
       <input type="submit" value="Insert" />
       <input type="reset"  value="Reset">
