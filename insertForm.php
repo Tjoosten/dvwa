5a9a2e5ea232b91aaecb2f2b18a4a8e08a101e08
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-06-18 18:55:21 +0200

    Create insertForm.php

---
+++ insertForm.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <title> DVWA - Insert Form </title>
+  </head>
+  <body>
+    <h1> DVWA - <i>(XSS via Request)</i> </h1>
+    
+    <form method="POST" action="/insertDB.php">
+      <input type="text" name="firstname" placeholder="firstname" />
+      <input type="text" name="lastname"  placeholder="lastname" />
+    
+      <input type="submit" value="Insert" />
+      <input type="reset"  value="Reset">
+    </form>
+  </ body>
+</html>
