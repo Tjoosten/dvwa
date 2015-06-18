7d8f947f94d239b48197702437198649e4e48388
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-06-18 19:09:02 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,9 @@
+--- DB name: root
+
+CREATE TABLE Users (
+  id INT(11) AUTO_INCREMENT, 
+  lastname VARCHAR(255),
+  lastname VARCHAR(255),
+  password VARCHAR(255), 
+  PRIMARY KEY(id))
+  ENGINE = MyISAM;
