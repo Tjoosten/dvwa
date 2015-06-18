b4966b7f2f27c72357b142f7c59a64a4246cd5cc
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-06-18 19:13:43 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,8 +2,14 @@
 
 CREATE TABLE Users (
   id INT(11) AUTO_INCREMENT, 
-  lastname VARCHAR(255),
+  firstname VARCHAR(255),
   lastname VARCHAR(255),
   password VARCHAR(255), 
   PRIMARY KEY(id))
   ENGINE = MyISAM;
+  
+INSERT Users ('','firstname','lastname','password') 
+  VALUES
+    ('','tim','joosten', '74cc1c60799e0a786ac7094b532f01b1'), 
+    ('','Admin','Administrator','456b7016a916a4b178dd72b947c152b7'), 
+    ('','User','User', 'e7df7cd2ca07f4f1ab415d457a6e1c13');
