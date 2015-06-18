c609d9207ddb91582e58f506966c32df69efd844
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-06-18 18:49:23 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <title> DVWA </title>
+  </head>
+  <body>
+    
+    <h1>DVWA</h1>
+  
+    <a href=""> MySQL Select <i>(all)</i> </a> <br />
+    <br></br>
+    
+    <a href=""> XSS <i>(url)</i> </br>
+  </body>
+</html>
